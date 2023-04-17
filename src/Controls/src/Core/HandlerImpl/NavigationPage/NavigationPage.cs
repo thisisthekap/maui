2c2e80c704105a2b7920639535db125f96e25e80
Matthew Leibowitz <mattleibow@live.com>
2023-04-17 17:42:57 +0200

    Missing some mappers

--- src/Controls/src/Core/HandlerImpl/NavigationPage/NavigationPage.cs
+++ src/Controls/src/Core/HandlerImpl/NavigationPage/NavigationPage.cs
@@ -1,8 +1,11 @@
 ﻿#nullable disable
+using System;
+
 namespace Microsoft.Maui.Controls
 {
 	public partial class NavigationPage
 	{
+		[Obsolete("Use NavigationViewHandler.Mapper instead.")]
 		public static IPropertyMapper<IStackNavigationView, NavigationViewHandler> ControlsNavigationPageMapper =
 			new PropertyMapper<NavigationPage, NavigationViewHandler>(NavigationViewHandler.Mapper)
 			{
@@ -15,7 +18,10 @@ namespace Microsoft.Maui.Controls
 		internal static new void RemapForControls()
 		{
 			// Adjust the mappings to preserve Controls.NavigationPage legacy behaviors
-			NavigationViewHandler.Mapper = ControlsNavigationPageMapper;
+#if IOS
+			NavigationViewHandler.Mapper.ReplaceMappingWhen<NavigationPage, NavigationViewHandler>(PlatformConfiguration.iOSSpecific.NavigationPage.PrefersLargeTitlesProperty.PropertyName, MapPrefersLargeTitles);
+			NavigationViewHandler.Mapper.ReplaceMappingWhen<NavigationPage, NavigationViewHandler>(PlatformConfiguration.iOSSpecific.NavigationPage.IsNavigationBarTranslucentProperty.PropertyName, MapIsNavigationBarTranslucent);
+#endif
 		}
 	}
 }
